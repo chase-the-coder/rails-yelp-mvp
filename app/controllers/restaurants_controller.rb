b79afb7c5a95832c156e8acfa4537f1253ec1959
Chase Lindsay <chase.lindsay1@gmail.com>
2020-02-19 14:23:14 -0300

    created new restaurant form and reviews controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,4 +6,22 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
   end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save!
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def restaurant_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
 end
