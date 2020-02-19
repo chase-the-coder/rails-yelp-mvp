75215f5d2a95182aad8c9a9ceb728ae07fc6ebd0
Chase Lindsay <chase.lindsay1@gmail.com>
2020-02-19 12:53:54 -0300

    created show in crud

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_145523) do
+ActiveRecord::Schema.define(version: 2020_02_19_150425) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_02_19_145523) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone_number"
   end
 
   create_table "reviews", force: :cascade do |t|
