cfbc01291916e831d55054e7105d9d82a9ddc7c3
Chase Lindsay <chase.lindsay1@gmail.com>
2020-02-19 16:29:01 -0300

    comleted exercise

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,5 @@ class Review < ApplicationRecord
   # When a restaurant is destroyed, all of its reviews should be destroyed as well.
   belongs_to :restaurant
   validates :content, presence: true
-  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }, numericality: { only_integer: true }, presence: true
 end
